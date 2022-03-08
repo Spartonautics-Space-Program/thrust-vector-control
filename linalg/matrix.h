0dab9924af66afdb44c09131fcd63667d2927229
milind-u <milind.upadhyay@gmail.com>
2022-03-08 08:08:29 -0800

    Improve matrix API
    Signed-off-by: milind-u <milind.upadhyay@gmail.com>

--- linalg/matrix.h
+++ linalg/matrix.h
@@ -2,16 +2,18 @@
 #define MATRIX_H_
 
 #include <array>
+#include <cmath>
 #include <iostream>
 
 namespace spartonautics::linalg {
 
+// Fixed size, stack-allocated dense matrix
 template <typename S, size_t Rows, size_t Cols>
 class Matrix {
  public:
-  typedef std::array<S, Cols> stl_row;
-  typedef std::array<S, Rows> stl_col;
-  typedef std::array<stl_row, Rows> stl_matrix;
+  typedef std::array<S, Cols> Row;
+  typedef std::array<S, Rows> Col;
+  typedef std::array<Row, Rows> Data;
 
   // Zeros the matrix by default
   constexpr Matrix() : data_() {
@@ -19,48 +21,35 @@ class Matrix {
       row.fill(S(0.0));
     }
   }
-  constexpr Matrix(const stl_matrix &mat) : data_(mat) {}
-  constexpr Matrix(const stl_col &vector) : Matrix() {
+  constexpr Matrix(const Data &mat) : data_(mat) {}
+
+  virtual void Print(std::ostream &os) const {
+    os << '[';
     for (size_t i = 0; i < Rows; i++) {
-      data_[i][0] = vector[i];
+      os << '[';
+      for (size_t j = 0; j < (Cols - 1); j++) {
+        os << at(i, j) << ", ";
+      }
+      os << at(i, Cols - 1) << ']';
+      if (i != Rows - 1) {
+        os << std::endl;
+      }
     }
+    os << ']';
   }
 
-  void Print(std::ostream &os) const {
-    if (Cols == 1) {  // Vector
-      os << '[';
-      for (size_t i = 0; i < Rows - 1; i++) {
-        os << at(i, 0) << ", ";
-      }
-      os << at(Rows - 1, 0) << ']';
-    } else {  // Matrix
-      os << '[';
-      for (size_t i = 0; i < Rows; i++) {
-        os << '[';
-        for (size_t j = 0; j < (Cols - 1); j++) {
-          os << at(i, j) << ", ";
-        }
-        os << at(i, Cols - 1) << ']';
-        if (i != Rows - 1) {
-          os << std::endl;
-        }
-      }
-      os << ']';
-    }
-  }
-
-  constexpr stl_matrix &data() { return data_; }
-  constexpr const stl_matrix &data() const { return data_; }
+  constexpr Data *mutable_data() { return data_; }
+  constexpr const Data &data() const { return data_; }
 
   constexpr S &operator()(const size_t i, const size_t j) {
     return data_[i][j];
   }
-  constexpr stl_row &operator[](const size_t i) const { return data_[i]; }
+  constexpr Row &operator[](const size_t i) const { return data_[i]; }
   constexpr S x() { return at(0, 0); }
   constexpr S y() { return at(1, 0); }
   constexpr S z() { return at(2, 0); }
 
-  constexpr const stl_row &row(const size_t i) const { return data_[i]; }
+  constexpr const Row &row(const size_t i) const { return data_[i]; }
   constexpr const S &at(const size_t i, const size_t j) const {
     return data_[i][j];
   }
@@ -68,10 +57,10 @@ class Matrix {
   constexpr S y() const { return at(1, 0); }
   constexpr S z() const { return at(2, 0); }
 
-  constexpr stl_matrix::iterator begin() { return data_.begin(); }
-  constexpr stl_matrix::const_iterator cbegin() const { return data_.cbegin(); }
-  constexpr stl_matrix::iterator end() { return data_.end(); }
-  constexpr stl_matrix::const_iterator cend() const { return data_.cend(); }
+  constexpr Data::iterator begin() { return data_.begin(); }
+  constexpr Data::const_iterator cbegin() const { return data_.cbegin(); }
+  constexpr Data::iterator end() { return data_.end(); }
+  constexpr Data::const_iterator cend() const { return data_.cend(); }
 
   constexpr bool operator==(const Matrix<S, Rows, Cols> &mat) const {
     bool equal = true;
@@ -130,6 +119,9 @@ class Matrix {
     return scaled;
   }
 
+ protected:
+  Data data_;
+
  private:
   constexpr void AddSubtract(const bool addition,
                              const Matrix<S, Rows, Cols> &src,
@@ -149,8 +141,49 @@ class Matrix {
       }
     }
   }
+};
 
-  stl_matrix data_;
+template <typename S, size_t Size>
+class Vector : public Matrix<S, Size, 1> {
+ public:
+  constexpr Vector() : Matrix<S, Size, 1>() {}
+  constexpr Vector(const Matrix<S, Size, 1>::Col &vec) {
+    for (size_t i = 0; i < Size; i++) {
+      Matrix<S, Size, 1>::data_[i][0] = vec[i];
+    }
+  }
+
+  virtual void Print(std::ostream &os) const override {
+    os << '[';
+    for (size_t i = 0; i < Size - 1; i++) {
+      os << Matrix<S, Size, 1>::at(i, 0) << ", ";
+    }
+    os << Matrix<S, Size, 1>::at(Size - 1, 0) << ']';
+  }
+
+  constexpr S &operator()(const size_t i) {
+    return Matrix<S, Size, 1>::data_[i][0];
+  }
+
+  constexpr const S &at(const size_t i) const {
+    return Matrix<S, Size, 1>::at(i, 0);
+  }
+
+  constexpr S Dot(const Vector<S, Size> &vec) const {
+    S dot = S(0.0);
+    for (size_t i = 0; i < Size; i++) {
+      dot += at(i) * vec.at(i);
+    }
+    return dot;
+  }
+
+  constexpr S Norm() const {
+    S norm = S(0.0);
+    for (size_t i = 0; i < Size; i++) {
+      norm += std::pow(at(i), S(2.0));
+    }
+    return std::sqrt(norm);
+  }
 };
 
 template <typename S, size_t Rows, size_t Cols>
@@ -168,9 +201,9 @@ constexpr Matrix<S, Rows, Cols> operator*(const S scalar,
 using Matrix2 = Matrix<double, 2, 2>;
 using Matrix3 = Matrix<double, 3, 3>;
 using Matrix4 = Matrix<double, 4, 4>;
-using Vector2 = Matrix<double, 2, 1>;
-using Vector3 = Matrix<double, 3, 1>;
-using Vector4 = Matrix<double, 3, 1>;
+using Vector2 = Vector<double, 2>;
+using Vector3 = Vector<double, 3>;
+using Vector4 = Vector<double, 3>;
 
 }  // namespace spartonautics::linalg
 
--- linalg/matrix_test.cc
+++ linalg/matrix_test.cc
@@ -7,7 +7,7 @@ namespace spartonautics::linalg::testing {
 
 TEST(MatrixTest, Iterating) {
   constexpr Matrix3 kM =
-      Matrix3::stl_matrix{{{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}}};
+      Matrix3::Data{{{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}}};
   constexpr std::array<double, 9> kData = {1.0, 2.0, 3.0, 4.0, 5.0,
                                            6.0, 7.0, 8.0, 9.0};
 
@@ -19,43 +19,55 @@ TEST(MatrixTest, Iterating) {
     }
   }
   EXPECT_EQ(data_it, kData.end());
+
+  data_it = kData.cbegin();
+  for (auto row : kM.data()) {
+    for (double scalar : row) {
+      EXPECT_EQ(scalar, *data_it);
+      data_it++;
+    }
+  }
+  EXPECT_EQ(data_it, kData.end());
 }
 
 TEST(MatrixTest, AddSubtract) {
-  constexpr Matrix3 kM1 = Matrix3::stl_matrix{
-      {{1.0, 2.0, 3.0}, {3.0, 2.0, 1.0}, {-1.0, -2.0, 3.0}}};
-  constexpr Matrix3 kM2 = Matrix3::stl_matrix{
-      {{-1.0, 3.0, 2.0}, {1.0, 2.0, 2.0}, {3.0, -2.0, 2.0}}};
+  constexpr Matrix3 kM1 =
+      Matrix3::Data{{{1.0, 2.0, 3.0}, {3.0, 2.0, 1.0}, {-1.0, -2.0, 3.0}}};
+  constexpr Matrix3 kM2 =
+      Matrix3::Data{{{-1.0, 3.0, 2.0}, {1.0, 2.0, 2.0}, {3.0, -2.0, 2.0}}};
   EXPECT_EQ(kM1 + kM2,
-            Matrix3(Matrix3::stl_matrix{
+            Matrix3(Matrix3::Data{
                 {{0.0, 5.0, 5.0}, {4.0, 4.0, 3.0}, {2.0, -4.0, 5.0}}}));
   EXPECT_EQ(kM1 - kM2,
-            Matrix3(Matrix3::stl_matrix{
+            Matrix3(Matrix3::Data{
                 {{2.0, -1.0, 1.0}, {2.0, 0.0, -1.0}, {-4.0, 0.0, 1.0}}}));
 }
 
 TEST(MatrixTest, Multiply) {
   constexpr Matrix<double, 2, 3> kM1 =
-      Matrix<double, 2, 3>::stl_matrix{{{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}}};
+      Matrix<double, 2, 3>::Data{{{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}}};
   constexpr Matrix<double, 3, 2> kM2 =
-      Matrix<double, 3, 2>::stl_matrix{{{7.0, 8.0}, {9.0, 10.0}, {11.0, 12.0}}};
-  EXPECT_EQ(kM1 * kM2,
-            Matrix2(Matrix2::stl_matrix{{{58.0, 64.0}, {139.0, 154.0}}}));
-  constexpr auto kScaled =
-      Matrix<double, 2, 3>(Matrix<double, 2, 3>::stl_matrix{
-          {{-2.0, -4.0, -6.0}, {-8.0, -10.0, -12.0}}});
+      Matrix<double, 3, 2>::Data{{{7.0, 8.0}, {9.0, 10.0}, {11.0, 12.0}}};
+  EXPECT_EQ(kM1 * kM2, Matrix2(Matrix2::Data{{{58.0, 64.0}, {139.0, 154.0}}}));
+  constexpr auto kScaled = Matrix<double, 2, 3>(
+      Matrix<double, 2, 3>::Data{{{-2.0, -4.0, -6.0}, {-8.0, -10.0, -12.0}}});
   constexpr auto kScaledResult = -2.0 * kM1;
   EXPECT_EQ(kScaledResult, kScaled);
 }
 
 TEST(MatrixTest, Vector) {
-  Vector3 v = Vector3::stl_col{{1.0, 2.0, 3.0}};
+  Vector3 v = Vector3::Col{{1.0, 2.0, 3.0}};
   EXPECT_EQ(v.x(), 1.0);
   EXPECT_EQ(v.y(), 2.0);
   EXPECT_EQ(v.z(), 3.0);
 
-  v(1, 0) = 4.0;
+  v(1) = 4.0;
   EXPECT_EQ(v.y(), 4.0);
+
+  EXPECT_DOUBLE_EQ(v.Norm(), std::sqrt(1.0 + 16.0 + 9.0));
+
+  constexpr Vector3 kV2 = Vector3::Col{{-2.0, 1.0, 5.0}};
+  EXPECT_DOUBLE_EQ(v.Dot(kV2), -2.0 + 4.0 + 15.0);
 }
 
 }  // namespace spartonautics::linalg::testing
